f3db4140ef72374c53b37e77de2cb7b2f8ab5dfc
Anton Golub <antongolub@antongolub.com>
2021-07-28 22:08:07 +0300

    test(client): destroy client afterAll

--- packages/client/src/test/ts/index.ts
+++ packages/client/src/test/ts/index.ts
@@ -3,9 +3,10 @@ import {createClient} from '../../main/ts'
 describe('@qiwi/ldap-client', () => {
   describe('createClient', () => {
     it('returns new LDAP client instance', async() => {
-      const client = await createClient({url: 'ldaps://ldap.qiwi.com'})
+      const client = await createClient({url: 'ldap://qiwi.com'})
 
       expect(client).not.toBeUndefined()
+      await client.destroy()
     })
   })
 })
